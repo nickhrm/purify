119903818a19fee858d2cae7070aa7d037336ab0
nickhrm <nickhrm2308@gmail.com>
2025-12-02 12:14:13 +0100

    c

--- src/purify/my_constants.py
+++ src/purify/my_constants.py
@@ -22,17 +22,15 @@ QUBIT_ARRIVAL_SCALE = P_G/(QUBIT_ENTANGLEMENT_FACTOR * DELTA_T)
 
 
 WAITING_TIME_SENSIVITIES = [
-    0.2,
-    1,
-    5,
-    10,
-    15,
-    20
+  1
 ]
 
-LAMBDA_1 = 0.0
-LAMBDA_2 = 0.3
-LAMBDA_3 = 0.0
+LAMBDA_1 = 0.1
+LAMBDA_2 = 0.2
+LAMBDA_3 = 0.05
+
+
+
 
 
 # Stratgies to include in the simulation
@@ -47,8 +45,8 @@ STRATEGIES = [
 
 # Decoherence times to include in the simulation in secs
 DECOHERENCE_TIMES = [
-         # 0.00001,
-        #0.0001,
+        # 0.00001,
+        # 0.0001,
         # 0.0003,
         # 0.0005,
         # 0.0008,
@@ -56,11 +54,11 @@ DECOHERENCE_TIMES = [
         # 0.003,
         # 0.005,
         # 0.008,
-         0.01,
+        0.01,
         # 0.05,
-         # 0.1,
+        # 0.1,
         # 0.5
-         # 1.0,
+        # 1.0,
     ]
 
 
